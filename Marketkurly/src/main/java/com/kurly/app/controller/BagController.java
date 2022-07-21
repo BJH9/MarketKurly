ec558e5508e3c6de049597991256de111d252d00
BJH9 <tfzangin9@gmail.com>
2022-07-21 19:55:52 +0900

    Create Update in CRUD

--- Marketkurly/src/main/java/com/kurly/app/controller/BagController.java
+++ Marketkurly/src/main/java/com/kurly/app/controller/BagController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -54,6 +55,12 @@ public class BagController {
 		List<Bag> list = dao.getList();
 		m.addAttribute("list", list);
 		return "shoppingbag";
+	}
+	
+	@RequestMapping(value = "/delete/{id}", method=RequestMethod.GET)
+	public String delete(@PathVariable int id) {
+		dao.delete(id);
+		return "redirect:/shoppingbag";
 	}
 	
 }
