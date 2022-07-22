43164cfe7719bfdb5d83b94efa90e6a0b1996f51
BJH9 <tfzangin9@gmail.com>
2022-07-22 13:15:22 +0900

    [Add] getter and setter

--- Marketkurly/src/main/java/com/kurly/app/beans/Item.java
+++ Marketkurly/src/main/java/com/kurly/app/beans/Item.java
@@ -5,4 +5,30 @@ public class Item {
 	private String itemName;
 	private String price;
 	private String content;
+	public int getItemId() {
+		return itemId;
+	}
+	public void setItemId(int itemId) {
+		this.itemId = itemId;
+	}
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
 }
